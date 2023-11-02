4282b6b0b2f27a355769baab24e3275d58a88c3d
Cyrille Derche <c.derche@me.com>
2023-11-02 14:53:17 +0100

    Add BullMQModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import validationSchema from './config/configs.schema';
 import baseConfig from './config/base.config';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
@@ -12,6 +13,16 @@ import baseConfig from './config/base.config';
       validationSchema: validationSchema,
       isGlobal: true,
       load: [baseConfig],
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
