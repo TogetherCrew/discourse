e7c48b739f0a69062cf6a5df685d0d4b841c38e9
Cyrille Derche <c.derche@me.com>
2023-11-02 15:58:49 +0100

    Create BadgeService

--- src/badge/badge.module.ts
+++ src/badge/badge.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { BADGE_QUEUE } from 'src/constants/queues.constants';
 import { BadgeProcessor } from './badge.processor';
+import { BadgeService } from './badge.service';
 
 @Module({
   imports: [BullModule.registerQueue({ name: BADGE_QUEUE })],
-  providers: [BadgeProcessor],
+  providers: [BadgeProcessor, BadgeService],
 })
 export class BadgeModule {}
