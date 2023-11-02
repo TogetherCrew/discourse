a28b65587c04d5d008420ca98308bf3a9341a0ea
Cyrille Derche <c.derche@me.com>
2023-11-02 17:31:34 +0100

    logic for badge-extract

---
+++ src/badge/handlers/badge-extract.handler.spec.ts
@@ -0,0 +1,99 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Queue } from 'bullmq';
+import { DiscourseService } from '@app/discourse';
+import { BadgeExtractHandler } from './badge-extract.handler';
+import {
+  BADGE_GROUPING_QUEUE,
+  BADGE_QUEUE,
+  BADGE_TYPE_QUEUE,
+} from '../../constants/queues.constants';
+import { TRANSFORM_JOB } from '../../constants/jobs.contants';
+import { AxiosResponse } from 'axios';
+import { BullModule } from '@nestjs/bullmq';
+
+const BullQueue_BADGE_QUEUE = `BullQueue_${BADGE_QUEUE}`;
+const BullQueue_BADGE_TYPE_QUEUE = `BullQueue_${BADGE_TYPE_QUEUE}`;
+const BullQueue_BADGE_GROUPING_QUEUE = `BullQueue_${BADGE_GROUPING_QUEUE}`;
+
+describe('BadgeExtractHandler', () => {
+  let handler: BadgeExtractHandler;
+  let discourseService: jest.Mocked<DiscourseService>;
+  let badgeQueue: jest.Mocked<Queue>;
+  let badgeTypeQueue: jest.Mocked<Queue>;
+  let badgeGroupingQueue: jest.Mocked<Queue>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        BadgeExtractHandler,
+        {
+          provide: DiscourseService,
+          useValue: {
+            getBadges: jest.fn(),
+          },
+        },
+        {
+          provide: BullQueue_BADGE_QUEUE,
+          useValue: {
+            add: jest.fn(),
+          },
+        },
+        {
+          provide: BullQueue_BADGE_TYPE_QUEUE,
+          useValue: {
+            add: jest.fn(),
+          },
+        },
+        {
+          provide: BullQueue_BADGE_GROUPING_QUEUE,
+          useValue: {
+            add: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    handler = module.get<BadgeExtractHandler>(BadgeExtractHandler);
+    discourseService = module.get(DiscourseService);
+    badgeQueue = module.get(BullQueue_BADGE_QUEUE);
+    badgeTypeQueue = module.get(BullQueue_BADGE_TYPE_QUEUE);
+    badgeGroupingQueue = module.get(BullQueue_BADGE_GROUPING_QUEUE);
+  });
+
+  it('should process job and queue badge transformations', async () => {
+    const mockJobData = {
+      forum: {
+        endpoint: 'testEndpoint',
+      },
+    };
+    const mockBadgeResponse: Partial<AxiosResponse<BadgesResponse, any>> = {
+      data: {
+        badges: [],
+        badge_types: [],
+        badge_groupings: [],
+      },
+    };
+    discourseService.getBadges.mockResolvedValue(
+      mockBadgeResponse as AxiosResponse<BadgesResponse>,
+    );
+    const job = { id: '1', data: mockJobData } as any;
+
+    await handler.process(job);
+
+    expect(discourseService.getBadges).toHaveBeenCalledWith(
+      mockJobData.forum.endpoint,
+    );
+    expect(badgeQueue.add).toHaveBeenCalledWith(TRANSFORM_JOB, {
+      forum: mockJobData.forum,
+      badges: mockBadgeResponse.data.badges,
+    });
+    expect(badgeTypeQueue.add).toHaveBeenCalledWith(TRANSFORM_JOB, {
+      forum: mockJobData.forum,
+      badge_types: mockBadgeResponse.data.badge_types,
+    });
+    expect(badgeGroupingQueue.add).toHaveBeenCalledWith(TRANSFORM_JOB, {
+      forum: mockJobData.forum,
+      badge_groupings: mockBadgeResponse.data.badge_groupings,
+    });
+  });
+});
--- src/badge/handlers/badge-extract.handler.ts
+++ src/badge/handlers/badge-extract.handler.ts
@@ -1,9 +1,38 @@
-import { Job } from 'bullmq';
+import { Job, Queue } from 'bullmq';
 import { Handler } from '../../abstracts/handler.abstract';
+import { DiscourseService } from '@app/discourse';
+import { InjectQueue } from '@nestjs/bullmq';
+import {
+  BADGE_GROUPING_QUEUE,
+  BADGE_QUEUE,
+  BADGE_TYPE_QUEUE,
+} from '../../constants/queues.constants';
+import { TRANSFORM_JOB } from '../../constants/jobs.contants';
+import { Logger } from '@nestjs/common';
 
 export class BadgeExtractHandler extends Handler {
-  process(job: Job<any, any, string>): Promise<any> {
-    console.log('BadgeExtractHandler', job.id);
-    throw new Error('Method not implemented.');
+  private readonly logger = new Logger(BadgeExtractHandler.name);
+
+  constructor(
+    private readonly discourseService: DiscourseService,
+    @InjectQueue(BADGE_QUEUE) private readonly badgeQueue: Queue,
+    @InjectQueue(BADGE_TYPE_QUEUE) private readonly badgeTypeQueue: Queue,
+    @InjectQueue(BADGE_GROUPING_QUEUE)
+    private readonly badgeGroupingQueue: Queue,
+  ) {
+    super();
+  }
+
+  async process(job: Job<any, any, string>): Promise<any> {
+    this.logger.log('BadgeExtractHandler', job.id);
+    const { forum } = job.data;
+    const { data } = await this.discourseService.getBadges(forum.endpoint);
+    const { badges, badge_types, badge_groupings } = data;
+    await this.badgeQueue.add(TRANSFORM_JOB, { forum, badges });
+    await this.badgeTypeQueue.add(TRANSFORM_JOB, { forum, badge_types });
+    await this.badgeGroupingQueue.add(TRANSFORM_JOB, {
+      forum,
+      badge_groupings,
+    });
   }
 }
