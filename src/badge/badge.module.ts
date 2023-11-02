de0319fed12773b040673aa5073a34adeef7226e
Cyrille Derche <c.derche@me.com>
2023-11-02 15:03:04 +0100

    Add BadgeProcessor

--- src/badge/badge.module.ts
+++ src/badge/badge.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { BADGE_QUEUE } from 'src/constants/queues';
+import { BadgeProcessor } from './badge.processor';
 
 @Module({
   imports: [BullModule.registerQueue({ name: BADGE_QUEUE })],
+  providers: [BadgeProcessor],
 })
 export class BadgeModule {}
