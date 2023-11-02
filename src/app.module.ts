586cb35123a1b5164463be88e49f3b22d5f9cd02
Mohammad Twin <scientists.twin@gmail.com>
2023-11-02 10:55:44 +0400

    feat: basic configuration for envs variable was added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import validationSchema from './config/configs.schema';
+import baseConfig from './config/base.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: validationSchema,
+      isGlobal: true,
+      load: [baseConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('base.port');
+
+  await app.listen(port, () => {
+    Logger.verbose(`Server is running on port ${port}`, 'NestApplication');
+  });
 }
 bootstrap();
