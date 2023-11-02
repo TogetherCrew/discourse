8357faa9365e663bbbdf78846255c41d7003e7e4
Mohammad Twin <scientists.twin@gmail.com>
2023-11-02 11:44:56 +0400

    feat: swagger was added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
     defaultVersion: '1',
     prefix: 'api/v',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Discourse API')
+    .setDescription('Documentation of the Discourse API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
 
   await app.listen(port, () => {
     Logger.verbose(`Server is running on port ${port}`, 'NestApplication');
