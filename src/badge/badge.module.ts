04f4ef90dc6195ac79edd26423d0185b4187180e
Cyrille Derche <c.derche@me.com>
2023-11-02 15:26:34 +0100

    rename queues

--- src/badge/badge.module.ts
+++ src/badge/badge.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
-import { BADGE_QUEUE } from 'src/constants/queues';
+import { BADGE_QUEUE } from 'src/constants/queues.constants';
 import { BadgeProcessor } from './badge.processor';
 
 @Module({
