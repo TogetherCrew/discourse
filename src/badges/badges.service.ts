ea3759cad1372ad7a1d463ac357fcb41326e699a
Cyrille Derche <c.derche@me.com>
2023-11-02 20:08:56 +0100

    badge service

--- src/badges/badges.service.ts
+++ src/badges/badges.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoadBadgeDto } from './dto/load-badges.dto';
+import { BadgesRepository } from './badges.repository';
 
 @Injectable()
-export class BadgesService {}
+export class BadgesService {
+  constructor(private readonly repository: BadgesRepository) {}
+
+  async insertMany(badges: LoadBadgeDto[]) {
+    return this.repository.insertMany(badges);
+  }
+}
