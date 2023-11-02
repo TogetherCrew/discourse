a9c9ab0a46ba5bedc5cb84490be0bbadc4e6a54d
Mohammad Twin <scientists.twin@gmail.com>
2023-11-02 10:57:11 +0400

    feat: versioning was added to the application

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('base.port');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'api/v',
+  });
 
   await app.listen(port, () => {
     Logger.verbose(`Server is running on port ${port}`, 'NestApplication');
