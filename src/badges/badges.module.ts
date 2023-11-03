1c35bd153a591823e413e1309c78777b1499ce3c
Cyrille Derche <c.derche@me.com>
2023-11-03 09:49:16 +0100

    Remove BadgesTransformer

--- src/badges/badges.module.ts
+++ src/badges/badges.module.ts
@@ -3,10 +3,9 @@ import { BullModule } from '@nestjs/bullmq';
 import { BADGE_QUEUE } from 'src/constants/queues.constants';
 import { BadgesProcessor } from './badges.processor';
 import { BadgesService } from './badges.service';
-import { BadgesTransformer } from './badges.transformer';
 
 @Module({
   imports: [BullModule.registerQueue({ name: BADGE_QUEUE })],
-  providers: [BadgesProcessor, BadgesService, BadgesTransformer],
+  providers: [BadgesProcessor, BadgesService],
 })
 export class BadgesModule {}
