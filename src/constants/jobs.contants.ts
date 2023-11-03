aa0b0aa70529748c2ac3db046f07b75744c46645
Cyrille Derche <c.derche@me.com>
2023-11-03 16:12:03 +0100

    Update jobs

--- src/constants/jobs.contants.ts
+++ src/constants/jobs.contants.ts
@@ -1,3 +1,10 @@
 export const EXTRACT_JOB = 'EXTRACT_JOB';
 export const TRANSFORM_JOB = 'TRANSFORM_JOB';
 export const LOAD_JOB = 'LOAD_JOB';
+
+export const JOBS = {
+  EXTRACT: 'EXTRACT',
+  TRANSFORM: 'TRANSFORM',
+  LOAD: 'LOAD',
+  RUN: 'RUN',
+};
