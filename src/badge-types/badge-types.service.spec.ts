40cdfe62bc1803bfb7ea98bd23a10b91f25dcf43
Cyrille Derche <c.derche@me.com>
2023-11-03 10:00:36 +0100

    Update BadgeTypesService

--- src/badge-types/badge-types.service.spec.ts
+++ src/badge-types/badge-types.service.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BadgeTypesService } from './badge-types.service';
+import { BadgeTypesRepository } from './badge-types.repository';
+import { LoadBadgeTypeDto } from '../badges/dto/load-badges.dto';
+
+jest.mock('./badge-types.repository');
 
 describe('BadgeTypesService', () => {
   let service: BadgeTypesService;
+  let repository: jest.Mocked<BadgeTypesRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BadgeTypesService],
+      providers: [BadgeTypesService, BadgeTypesRepository],
     }).compile();
 
     service = module.get<BadgeTypesService>(BadgeTypesService);
+    repository = module.get(BadgeTypesRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('insertMany', () => {
+    it('should call the repository insertMany method with the provided badges', async () => {
+      const mockBadges = [
+        { name: 'test-badge-1', forumUUID: 'uuid-1' },
+        { name: 'test-badge-2', forumUUID: 'uuid-2' },
+        // Add more badges as needed
+      ] as LoadBadgeTypeDto[];
+
+      await service.insertMany(mockBadges);
+
+      expect(repository.insertMany).toHaveBeenCalledWith(mockBadges);
+    });
   });
 });
--- src/badge-types/badge-types.service.ts
+++ src/badge-types/badge-types.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoadBadgeTypeDto } from '../badges/dto/load-badges.dto';
+import { BadgeTypesRepository } from './badge-types.repository';
 
 @Injectable()
-export class BadgeTypesService {}
+export class BadgeTypesService {
+  constructor(private readonly repository: BadgeTypesRepository) {}
+
+  async insertMany(badgeTypes: LoadBadgeTypeDto[]) {
+    return this.repository.insertMany(badgeTypes);
+  }
+}
