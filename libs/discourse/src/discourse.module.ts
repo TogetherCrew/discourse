4e6258446969c818d6e6a3edbc16af6537a3a365
Cyrille Derche <c.derche@me.com>
2023-11-03 15:04:34 +0100

    Fix #25

--- libs/discourse/src/discourse.module.ts
+++ libs/discourse/src/discourse.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DiscourseService } from './discourse.service';
 import { BottleneckModule } from './bottleneck/bottleneck.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   providers: [DiscourseService],
   exports: [DiscourseService],
-  imports: [BottleneckModule],
+  imports: [BottleneckModule, HttpModule],
 })
 export class DiscourseModule {}
--- src/badges/badges.module.ts
+++ src/badges/badges.module.ts
@@ -1,12 +1,36 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
-import { BADGE_QUEUE } from 'src/constants/queues.constants';
+import {
+  BADGE_GROUPING_QUEUE,
+  BADGE_QUEUE,
+  BADGE_TYPE_QUEUE,
+} from 'src/constants/queues.constants';
 import { BadgesProcessor } from './badges.processor';
 import { BadgesService } from './badges.service';
 import { BadgesTransformer } from './badges.transformer';
+import { BadgesExtractHandler } from './handlers/badges-extract.handler';
+import { BadgesTransformHandler } from './handlers/badges-transform.handler';
+import { BadgesLoadHandler } from './handlers/badges-load.handler';
+import { BadgesRepository } from './badges.repository';
+import { DiscourseModule } from '@app/discourse';
 
 @Module({
-  imports: [BullModule.registerQueue({ name: BADGE_QUEUE })],
-  providers: [BadgesProcessor, BadgesService, BadgesTransformer],
+  imports: [
+    BullModule.registerQueue(
+      { name: BADGE_QUEUE },
+      { name: BADGE_TYPE_QUEUE },
+      { name: BADGE_GROUPING_QUEUE },
+    ),
+    DiscourseModule,
+  ],
+  providers: [
+    BadgesProcessor,
+    BadgesService,
+    BadgesTransformer,
+    BadgesExtractHandler,
+    BadgesTransformHandler,
+    BadgesLoadHandler,
+    BadgesRepository,
+  ],
 })
 export class BadgesModule {}
