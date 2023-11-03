848fcfa8ff3f3a4975ea08bc50178d96f59b14ff
Cyrille Derche <c.derche@me.com>
2023-11-03 10:14:16 +0100

    Add new services to BadgeLoadHandlers

--- src/badges/handlers/badges-load.handler.ts
+++ src/badges/handlers/badges-load.handler.ts
@@ -3,16 +3,24 @@ import { Handler } from '../../abstracts/handler.abstract';
 import { LoadBadgesDto } from '../dto/load-badges.dto';
 import { BadgesService } from '../badges.service';
 import { Injectable } from '@nestjs/common';
+import { BadgeTypesService } from '../../badge-types/badge-types.service';
+import { BadgeGroupingsService } from '../../badge-groupings/badge-groupings.service';
 
 @Injectable()
 export class BadgesLoadHandler extends Handler {
-  constructor(public readonly badgeService: BadgesService) {
+  constructor(
+    private readonly badgesService: BadgesService,
+    private readonly badgeTypesService: BadgeTypesService,
+    private readonly badgeGroupingsService: BadgeGroupingsService,
+  ) {
     super();
   }
 
   async process(job: Job<LoadBadgesDto, any, string>): Promise<any> {
     console.log('BadgesLoadHandler', job.id);
-    const { badges } = job.data;
-    await this.badgeService.insertMany(badges);
+    const { badges, badgeTypes, badgeGroupings } = job.data;
+    await this.badgeTypesService.insertMany(badgeTypes);
+    await this.badgeGroupingsService.insertMany(badgeGroupings);
+    await this.badgesService.insertMany(badges);
   }
 }
