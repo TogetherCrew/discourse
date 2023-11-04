2c68e1cf79ecbd4c2ec956ab5552e07f98421db3
Cyrille Derche <c.derche@me.com>
2023-11-04 14:30:26 +0100

    Update forum for orchestration

--- src/forums/forums.module.ts
+++ src/forums/forums.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ForumsService } from './forums.service';
 import { ForumsController } from './forums.controller';
 import { ForumsRepository } from './forums.repository';
+import { OrchestrationModule } from 'src/orchestration/orchestration.module';
 
 @Module({
+  imports: [OrchestrationModule],
   controllers: [ForumsController],
   providers: [ForumsService, ForumsRepository],
 })
