11c4493d01773d308e1cdf43764ffa294f9dd7fa
Cyrille Derche <c.derche@me.com>
2023-11-05 17:25:40 +0100

    add topics processor

--- src/topics/topics.module.ts
+++ src/topics/topics.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TopicsProcessor } from './topics.processor';
+import { BullModule } from '@nestjs/bullmq';
+import { BaseEtlModule } from '../base-etl/base-etl.module';
+import { QUEUES } from '../constants/queues.constants';
+import { TopicsEtlService } from './topics-etl.service';
+import { DiscourseModule } from '@app/discourse';
+import { BaseTransformerModule } from 'src/base-transformer/base-transformer.module';
 
-@Module({})
+@Module({
+  imports: [
+    BullModule.registerQueue({ name: QUEUES.TOPIC }),
+    BaseEtlModule,
+    DiscourseModule,
+    BaseTransformerModule,
+  ],
+  providers: [TopicsProcessor, TopicsEtlService],
+})
 export class TopicsModule {}
