54e69a49a61e0a17215bff8c22c8f49695fba13e
Cyrille Derche <c.derche@me.com>
2023-11-05 13:10:29 +0100

    Update BadgeModule

--- src/badges/badges.module.ts
+++ src/badges/badges.module.ts
@@ -1,36 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
-import {
-  BADGE_GROUPING_QUEUE,
-  BADGE_QUEUE,
-  BADGE_TYPE_QUEUE,
-} from 'src/constants/queues.constants';
+import { QUEUES } from 'src/constants/queues.constants';
 import { BadgesProcessor } from './badges.processor';
-import { BadgesService } from './badges.service';
-import { BadgesTransformer } from './badges.transformer';
-import { BadgesExtractHandler } from './handlers/badges-extract.handler';
-import { BadgesTransformHandler } from './handlers/badges-transform.handler';
-import { BadgesLoadHandler } from './handlers/badges-load.handler';
-import { BadgesRepository } from './badges.repository';
-import { DiscourseModule } from '@app/discourse';
+import { BaseEtlModule } from 'src/base-etl/base-etl.module';
 
 @Module({
-  imports: [
-    BullModule.registerQueue(
-      { name: BADGE_QUEUE },
-      { name: BADGE_TYPE_QUEUE },
-      { name: BADGE_GROUPING_QUEUE },
-    ),
-    DiscourseModule,
-  ],
-  providers: [
-    BadgesProcessor,
-    BadgesService,
-    BadgesTransformer,
-    BadgesExtractHandler,
-    BadgesTransformHandler,
-    BadgesLoadHandler,
-    BadgesRepository,
-  ],
+  imports: [BullModule.registerQueue({ name: QUEUES.BADGE }), BaseEtlModule],
+  providers: [BadgesProcessor],
 })
 export class BadgesModule {}
--- src/badges/badges.processor.spec.ts
+++ src/badges/badges.processor.spec.ts
@@ -1,75 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BadgesExtractHandler } from './handlers/badges-extract.handler';
-import { BadgesLoadHandler } from './handlers/badges-load.handler';
-import { BadgesTransformHandler } from './handlers/badges-transform.handler';
-import { Job } from 'bullmq';
-import {
-  EXTRACT_JOB,
-  LOAD_JOB,
-  TRANSFORM_JOB,
-} from '../constants/jobs.contants';
 import { BadgesProcessor } from './badges.processor';
+import { BaseEtlService } from '../base-etl/base-etl.service';
 
-describe('BadgesProcessor', () => {
+jest.mock('../base-etl/base-etl.service');
+
+describe('BadgeGroupingsService', () => {
   let processor: BadgesProcessor;
-  let extractHandler: jest.Mocked<BadgesExtractHandler>;
-  let transformHandler: jest.Mocked<BadgesTransformHandler>;
-  let loadHandler: jest.Mocked<BadgesLoadHandler>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        BadgesProcessor,
-        {
-          provide: BadgesExtractHandler,
-          useValue: {
-            process: jest.fn(),
-          },
-        },
-        {
-          provide: BadgesTransformHandler,
-          useValue: {
-            process: jest.fn(),
-          },
-        },
-        {
-          provide: BadgesLoadHandler,
-          useValue: {
-            process: jest.fn(),
-          },
-        },
-      ],
+      providers: [BadgesProcessor, BaseEtlService],
     }).compile();
 
     processor = module.get<BadgesProcessor>(BadgesProcessor);
-    extractHandler = module.get(BadgesExtractHandler);
-    transformHandler = module.get(BadgesTransformHandler);
-    loadHandler = module.get(BadgesLoadHandler);
   });
 
-  it('should process EXTRACT_JOB', async () => {
-    const job = { name: EXTRACT_JOB } as Job<any, any, string>;
-    await processor.process(job);
-    expect(extractHandler.process).toHaveBeenCalledWith(job);
-  });
-
-  it('should process TRANSFORM_JOB', async () => {
-    const job = { name: TRANSFORM_JOB } as Job<any, any, string>;
-    await processor.process(job);
-    expect(transformHandler.process).toHaveBeenCalledWith(job);
-  });
-
-  it('should process LOAD_JOB', async () => {
-    const job = { name: LOAD_JOB } as Job<any, any, string>;
-    await processor.process(job);
-    expect(loadHandler.process).toHaveBeenCalledWith(job);
-  });
-
-  it('should not process unknown jobs', async () => {
-    const job = { name: 'UNKNOWN_JOB' } as Job<any, any, string>;
-    await processor.process(job);
-    expect(extractHandler.process).not.toHaveBeenCalled();
-    expect(transformHandler.process).not.toHaveBeenCalled();
-    expect(loadHandler.process).not.toHaveBeenCalled();
+  it('should be defined', () => {
+    expect(processor).toBeDefined();
   });
 });
