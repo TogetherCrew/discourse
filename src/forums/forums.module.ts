2f7ca950dd31d9e3e2ec4c1ebb132fab75fbf297
Cyrille Derche <c.derche@me.com>
2023-11-15 17:48:11 +0100

    Fix Forum

--- src/forums/forums.module.ts
+++ src/forums/forums.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ForumsService } from './forums.service';
 import { ForumsController } from './forums.controller';
 import { ForumsRepository } from './forums.repository';
-import { OrchestrationModule } from 'src/orchestration/orchestration.module';
+import { OrchestrationModule } from '../orchestration/orchestration.module';
 
 @Module({
   imports: [OrchestrationModule],
